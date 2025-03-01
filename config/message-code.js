cb793bc4ddfd4d2351db7e8d7a5ed61ec3a0ad19
Siddhesh Shinde <siddheshshinde@Siddheshs-Air.bwrouter>
2025-03-01 11:55:51 +0530

    add success key in response

--- config/message-code.js
+++ config/message-code.js
@@ -3,6 +3,7 @@ const MESSAGE_CODE = {
   "SUCCESS": {
     statusCode: "EXP-2000",
     status: 200,
+    success: true,
     message: "Success"
   },
 
@@ -10,31 +11,37 @@ const MESSAGE_CODE = {
   "BAD_REQUEST": {
     statusCode: "EXP-4000",
     status: 400,
+    success: false,
     message: "Bad Request"
   },
   "VALIDATION_ERROR": {
     statusCode: "EXP-4002",
     status: 422,
+    success: false,
     message: "Validation Error"
   },
   "ALREADY_EXIST": {
     statusCode: "EXP-4003",
     status: 404,
+    success: false,
     message: "Data is already exist"
   },
   "NOT_FOUND": {
     statusCode: "EXP-4004",
     status: 404,
+    success: false,
     message: "Not Found"
   },
   "USER_REPORTED": {
     statusCode: "EXP-4005",
     status: 404, // confirm this status code
+    success: false,
     message: "User is reported for misusing the application"
   },
   "USER_DEACTIVATED": {
     statusCode: "EXP-4006",
     status: 404, // confirm this status code
+    success: false,
     message: "User is deactivated"
   },
 
@@ -42,16 +49,19 @@ const MESSAGE_CODE = {
   "UNAUTHORIZED_ERROR": {
     statusCode: "EXP-4001",
     status: 401,
+    success: false,
     message: "Unauthorized Access"
   },
   "INVALID_EMAIL_OR_PASSWORD": {
     statusCode: "EXP-4002",
     status: 401,
+    success: false,
     message: "Invalid email or password"
   },
   "OTP_EXPIRY": {
     statusCode: "EXP-4003",
     status: 401,
+    success: false,
     message: "OTP expiry"
   },
 
@@ -60,6 +70,7 @@ const MESSAGE_CODE = {
   "INTERNAL_ERROR": {
     statusCode: "EXP-5000",
     status: 500,
+    success: false,
     message: "Failed"
   }
 }
