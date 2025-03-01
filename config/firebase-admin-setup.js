e10ab3db85b04cbe441fe63dc30d2a9e2bd08c95
Siddhesh Shinde <siddheshshinde@Siddheshs-Air.bwrouter>
2025-03-02 00:44:07 +0530

    env key changes of firebase

--- config/firebase-admin-setup.js
+++ config/firebase-admin-setup.js
@@ -1,8 +1,23 @@
 const admin = require('firebase-admin');
-const serviceAccount = require("./../serviceAccountKey.json");
+// const serviceAccount = require("./../serviceAccountKey.json");
 const { initializeApp } = require('firebase/app');
 const { getAuth } = require('firebase/auth');
 const { getFirestore } = require('firebase/firestore');
+
+const serviceAccount = {
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url,
+  "universe_domain": process.env.universe_domain
+}
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
