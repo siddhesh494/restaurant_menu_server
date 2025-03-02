40177fb5aaab3076a11c577af70e6a9b0edb1ded
Siddhesh Shinde <siddheshshinde@Siddheshs-Air.bwrouter>
2025-03-02 15:41:40 +0530

    change api route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const routeV1 = require("./v1")
 
 async function bindRoutes(app) {
   routeV1.forEach(item => {
-    app.use(`/.netlify/api/v1/${item.route}`, item.dir)
+    app.use(`/api/v1/${item.route}`, item.dir)
   });
   return Promise.resolve()
 }
