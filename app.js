80739117f7aa30b114cb01827cf3d5ac2d903e0c
Siddhesh Shinde <siddheshshinde@Siddheshs-Air.bwrouter>
2025-03-02 15:55:30 +0530

    fix cors error => change baseurl

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var app = express();
 // app.set('view engine', 'ejs');
 
 
-app.options("*", cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
+app.options("*", cors({ origin: 'https://restaurant-menu-server.onrender.com', optionsSuccessStatus: 200 }));
 
-app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
+app.use(cors({ origin: "https://restaurant-menu-server.onrender.com", optionsSuccessStatus: 200 }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
